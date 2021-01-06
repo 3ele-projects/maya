dabdabac8289830048d84ba1d66001ef8934700c
Your Name <you@example.com>
2021-01-06 19:16:10 +0100

    add fixes

--- assets/assets.php
+++ assets/assets.php
@@ -1,7 +1,8 @@
 <?php 
 function lwmedia_include_styles() {
         wp_enqueue_script( 'momentjs', plugin_dir_url( __FILE__ ) .'node_modules/moment/moment.js', array( 'jquery' ) );
-		wp_enqueue_script( 'fullcalendar', plugin_dir_url( __FILE__ ) .'node_modules/fullcalendar-year-view/dist/fullcalendar.js', array( 'jquery' ) );
+        wp_enqueue_script( 'fullcalendar', plugin_dir_url( __FILE__ ) .'node_modules/fullcalendar-year-view/dist/fullcalendar.js', array( 'jquery' ) );
+        wp_enqueue_script( 'fullcalendarlocal', plugin_dir_url( __FILE__ ) .'node_modules/fullcalendar-year-view/dist/locale-all.js', array( 'jquery' ) );
 		wp_enqueue_script( 'fullcalendar-templates', plugin_dir_url( __FILE__ ) . 'js/fullcalendar-templates.js', array( 'jquery' ) );
 		wp_enqueue_script( 'accordionjs', plugin_dir_url( __FILE__ ) . 'js/bootstrap.js', array( 'jquery' ) );
 
--- event.php
+++ event.php
@@ -3,6 +3,7 @@
 class MayaCalender {
 
     public function build_day($color_ID, $startPoint){
+
         $aufwaermuebung = get_posts_from_color(get_field('color',$color_ID), 'aufwaermuebung');
         $atemuebung = get_posts_from_color(get_field('color',$color_ID), 'atemuebung');
   
@@ -13,10 +14,11 @@ class MayaCalender {
         $cal_event = array();
         $cal_event['id'] = $color_ID;
         $cal_event['start'] = get_field('date',$color_ID);
-   
+  //  var_dump($cal_event);
         $cal_event['classNames'] = get_field('color',$color_ID);
         $cal_event['classNames'] = get_field('color',$color_ID);
         $cal_event['seal'] = $image;
+		$cal_event['color_title'] = get_the_title($color_ID);
         $cal_event['seal_id'] = sealoftheday(get_field('date',$color_ID), $startPoint);
         $cal_event['workout_title'] = get_the_title($workout_id);
         $cal_event['workout_content'] = \Elementor\Plugin::$instance->frontend->get_builder_content_for_display($workout_id, true );
